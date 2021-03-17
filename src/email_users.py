d93a54800c1a9080151c766863ac5dde0e2c72d1
adoezema <austindoezema@gmail.com>
2021-03-17 15:16:48 -0400

    INITIAL COMMIT OF EMAIL USERS

---
+++ src/email_users.py
@@ -0,0 +1,9 @@
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from smtplib import SMTP
+import smtplib
+import sys
+
+#TODO: Email Users Results Query
+#TODO: email_users.py should be imported into main
