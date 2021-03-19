8b4ae2ce01659bea190f7c8aa8e19c1bceb664c6
adoezema <austindoezema@gmail.com>
2021-03-19 15:12:59 -0400

    test: VERIFIES SMTP SERVER WORKS
    Test that SMPT Server works with enviorment variables.
    Sends test emial to TEST@EMAIL.COM.
    Will have to change test email address.

--- src/email_users.py
+++ src/email_users.py
@@ -1,9 +1,22 @@
-from email.mime.text import MIMEText
-from email.mime.application import MIMEApplication
-from email.mime.multipart import MIMEMultipart
-from smtplib import SMTP
+import os
 import smtplib
-import sys
 
-#TODO: Email Users Results Query
-#TODO: email_users.py should be imported into main
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+
+if __name__ =="__main__":
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo() 
+
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        
+        subject = 'PYTHON TEST EMAIL'
+        body = 'This is a test emial.'
+
+        msg = f'Subject: {subject}\n\n{body}'
+
+        smtp.sendmail(EMAIL_ADDRESS, 'TEST@EMAIL.COM', msg)
+    
