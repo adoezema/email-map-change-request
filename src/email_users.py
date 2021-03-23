5c514559bb5d065ee85963b23835ba7a5686f079
adoezema <austindoezema@gmail.com>
2021-03-23 15:52:18 -0400

    refactor: USES EMIAL & MIME MODULE
    Updates script to use the emial mime module for better email handeling
    with html formatting.

--- src/email_users.py
+++ src/email_users.py
@@ -1,22 +1,27 @@
 import os
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 
 EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
+to_users = ['TEST_EMAIL@emial.com']
+
+msg = MIMEMultipart()
+msg['Subject'] = 'Map Change Request Report'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = ', '.join(to_users)
+
+body = '<h1>Test</h1>'
+
+msg.attach(MIMEText(body, 'html'))
+
 if __name__ =="__main__":
-    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-        smtp.ehlo()
-        smtp.starttls()
-        smtp.ehlo() 
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp: 
 
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         
-        subject = 'PYTHON TEST EMAIL'
-        body = 'This is a test emial.'
-
-        msg = f'Subject: {subject}\n\n{body}'
-
-        smtp.sendmail(EMAIL_ADDRESS, 'TEST@EMAIL.COM', msg)
+        smtp.sendmail(EMAIL_ADDRESS, to_users, msg.as_string())
     
